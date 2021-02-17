a347d26af3b8e50d924605c430ba72f6dc75a14b
Victor Forbes <vhforbes@gmail.com>
2021-02-17 13:20:42 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import './App.css';
 
 import HomePage from './pages/HomePage/HomePage'
 import ShopPage from './pages/ShopPage/ShopPage';
-
-
-const HatsPage = () => (
-  <div>
-    HatsPage
-  </div>
-)
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
